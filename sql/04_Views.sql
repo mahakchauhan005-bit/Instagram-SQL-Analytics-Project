7a99393f0af1678bc79b643a5c56972e50e7a36c
mahakchauhan005-bit <mahakchauhan005@gmail.com>
2026-08-12 14:44:49 +0530

    Create 04_Views.sql

---
+++ sql/04_Views.sql
@@ -0,0 +1,6 @@
+-- =====================================================
+-- File: 04_Views.sql
+-- Project: Instagram SQL Analytics Project
+-- Purpose: Create analytical SQL views
+-- Author: Mahak Chauhan
+-- =====================================================
